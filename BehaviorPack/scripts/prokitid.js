d36862420872e61ba63f8e0e645c547e3098fc50
AlvinQID <github@alvinq.my.id>
2025-06-27 22:40:50 +0700

    see changelog.txt

--- BehaviorPack/scripts/prokitid.js
+++ BehaviorPack/scripts/prokitid.js
@@ -1,13 +1,34 @@
 import * as mcs from "@minecraft/server";
 import * as mcsui from "@minecraft/server-ui";
+import {
+  system,
+  CommandPermissionLevel,
+  CustomCommandParamType,
+  CustomCommandStatus
+} from "@minecraft/server";
 
-var module = {
-  server: mcs,
-  server-ui: mcsui,
-};
+system.beforeEvents.startup.subscribe((init) => {
+  const argsEnum = ["maker","name"];
+  init.customCommandRegistry.registerEnum("prokitid:args", argsEnum);
+
+  const infoCmd = {
+    name: "prokitid:info",
+    description: "Information Of This Add-on",
+    permissionLevel: CommandPermissionLevel.Admin,
+    cheatsRequired: false
+  };
+
+  init.customCommandRegistry.registerCommand(infoCmd, infoHandler);
+});
+
+function infoHandler(origin) {
+  console.log(JSON.stringify(origin));
+  return { status: CustomCommandStatus.Success };
+}
 
 const init = async () => {
   console.log("init function called");
+  //console.log("prokitid.scripts.fn");
 
 }
 
